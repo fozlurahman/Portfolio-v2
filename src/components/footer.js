aa5ed29d8f797eb785672ecf5d67e38dc40f3177
fozlurahman <77914292+fozlurahman@users.noreply.github.com>
2024-05-30 11:20:31 -0600

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -77,7 +77,7 @@ const Footer = () => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
-    fetch('https://api.github.com/repos/shahariaryan')
+    fetch('https://api.github.com/repos/fozlurahman')
       .then(response => response.json())
       .then(json => {
         const { stargazers_count, forks_count } = json;
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/shahariaryan">
+        <a href="https://github.com/fozlurahman">
           <div>Designed &amp; Built by Fozlur Rahman</div>
 
           {githubInfo.stars && githubInfo.forks && (
